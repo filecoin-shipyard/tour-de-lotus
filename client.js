4e2a5311a712473d6ad47a341c024daaab08f168
Jim Pick <jim@protocol.ai>
2020-04-17 20:45:54 -0700

    Run a single client

---
+++ client.js
@@ -0,0 +1,11 @@
+import LotusRPC from './lotus-client-rpc';
+import BrowserProvider from './lotus-client-provider-browser';
+import schema from '@filecoin-shipyard/lotus-client-schema/prototype/testnet-v3.js';
+
+console.log('Jim starting client')
+const api = 'lotus.testground.ipfs.team/api'
+const wsUrl = 'wss://' + api + `/1/node/rpc/v0`
+const provider = new BrowserProvider(wsUrl)
+const client = new LotusRPC(provider, { schema })
+
+export default client
--- test-external-3.js
+++ test-external-3.js
@@ -1,45 +1,15 @@
-function useLotusClient () {
-  const [client, setClient] = useState()
-
-  useEffect(() => {
-    console.log('Jim starting client')
-    const api = 'lotus.testground.ipfs.team/api'
-    const wsUrl = 'wss://' + api + `/1/node/rpc/v0`
-    const provider = new BrowserProvider(wsUrl)
-    const client = new LotusRPC(provider, { schema })
-    setClient(client)
-    return cleanup
-
-    async function cleanup () {
-      await client.destroy()
-    }
-  }, [])
-
-  return client
-}
-
-function useHeight (client) {
+() => {
   const [height, setHeight] = useState()
 
   useEffect(() => {
     if (!client) return
     setHeight("Loading...")
-    async function run () {
+    ;(async function run () {
       const result = await client.ChainHead()
       setHeight(result.Height)
       setTimeout(run, 1000)
-    }
-    run()
+    })()
   }, [client])
-
-  return height
-}
-
-const ChainHeight = () => {
-  const client = useLotusClient()
-  const height = useHeight(client)
 
   return <h1>{height}</h1>
 }
-
-render(<ChainHeight />)
