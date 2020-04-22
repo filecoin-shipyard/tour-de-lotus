00a84131b07e9909dddd6103281dad1f7b9d7912
Jim Pick <jim@protocol.ai>
2020-04-21 20:25:05 -0700

    Show total miner power

---
+++ state/power-all.js
@@ -0,0 +1,24 @@
+() => {
+  const [totalPower, setTotalPower] = useState()
+
+  useEffect(() => {
+    let state = { canceled: false }
+    if (tourContext.index !== slideIndex) return
+    setTotalPower('Loading...')
+    ;(async function run () {
+      if (state.canceled) return
+      const result = await client.stateMinerPower('<empty>', [])
+      if (state.canceled) return
+      setTotalPower(result.TotalPower)
+    })()
+    return () => { state.canceled = true }
+  }, [tourContext.index])
+
+  return (
+    <div>
+      <h2>Total Network Power</h2>
+      (Using StateMinerPower)
+      <h1>{totalPower} bytes</h1>
+    </div>
+  )
+}
